52b5ae0fcfe560213e2c0ef895d4ab675f37184a
Ian J.Um <31336310+code0xff@users.noreply.github.com>
2018-12-07 16:44:23 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import Header from './Header';
 import Main from './Main';
 
 class App extends Component {
+  state={selected: 0}
+  menuList = ['create', 'asset']
+  _menuSelect = (e) => {
+    this.setState({selected: parseInt(e.target.id)});
+  }
+
   render() {
     return (
       <div>
-        <Header />
-        <Main />
+        <Header menuSelect={this._menuSelect}/>
+        {this.state.selected === 0 ? <Main /> : <div style={{color: 'red', textAlign: 'center'}}>menu is not selected yet.</div>}
       </div>
     );
   }
