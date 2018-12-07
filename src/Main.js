bb2cfd5d1f5f16ff8f643f0b3010333ddfd5026d
Ian J.Um <31336310+code0xff@users.noreply.github.com>
2018-12-07 16:21:56 +0900

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import Header from './Header';
 import Map from './Map';
 import Menu from './Menu';
 
 class Main extends Component {
-  state = { row: 5, col: 5, width: 300, height: 300}
+  state = { row: 5, col: 5, width: 300, height: 300, menu: 'create'}
 
   _setRow = (rowSize) => {
     this.setState({row: rowSize});
@@ -20,36 +19,33 @@ class Main extends Component {
   }
    render() {
     return (
-      <div>
-        <Header />
-        <div style={{paddingTop: '50px'}}>
-          <table style={{margin: 'auto'}}>
-            <tbody>
-              <tr>
-                <td>
-                  <Map 
-                  row={this.state.row} 
-                  col={this.state.col} 
-                  width={this.state.width}
-                  height={this.state.height}
-                  />
-                </td>
-                <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-                <td><Menu
-                  row={this.state.row} 
-                  col={this.state.col} 
-                  width={this.state.width}
-                  height={this.state.height}
-                  border={this.state.border}
-                  setRow={this._setRow}
-                  setCol={this._setCol}
-                  setWidth={this._setWidth}
-                  setHeight={this._setHeight}
-                  /></td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+      <div style={{paddingTop: '50px'}}>
+        <table style={{margin: 'auto'}}>
+          <tbody>
+            <tr>
+              <td>
+                <Map 
+                row={this.state.row} 
+                col={this.state.col} 
+                width={this.state.width}
+                height={this.state.height}
+                />
+              </td>
+              <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+              <td><Menu
+                row={this.state.row} 
+                col={this.state.col} 
+                width={this.state.width}
+                height={this.state.height}
+                border={this.state.border}
+                setRow={this._setRow}
+                setCol={this._setCol}
+                setWidth={this._setWidth}
+                setHeight={this._setHeight}
+                /></td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     )
   }
