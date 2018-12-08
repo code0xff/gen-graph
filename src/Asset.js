3221d1ef747219676b5a1d7c58631ea4232818d5
Ian J.Um <31336310+code0xff@users.noreply.github.com>
2018-12-08 11:42:44 +0900

    Update Asset.js

---
+++ src/Asset.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import AddAsset from './AddAsset';
+import AssetList from './AssetList';
+
+class Asset extends Component {
+  render() {
+    return (
+      <div style={{paddingTop: '50px'}}>
+        <table style={{margin: 'auto'}}>
+          <tbody>
+            <tr>
+              <td>
+                <AddAsset />
+              </td>
+              <td>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              </td>
+              <td>
+                <AssetList />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
+
+export default Asset;
