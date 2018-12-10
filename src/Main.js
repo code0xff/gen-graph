e11f2dbded782cc5396a8f7dd112f051c2138f05
Ian J.Um <31336310+code0xff@users.noreply.github.com>
2018-12-10 13:51:16 +0900

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -3,21 +3,7 @@ import Map from './Map';
 import Menu from './Menu';
 
 class Main extends Component {
-  state = { row: 5, col: 5, width: 300, height: 300}
-
-  _setRow = (rowSize) => {
-    this.setState({row: rowSize});
-  }
-  _setCol = (colSize) => {
-    this.setState({col: colSize});
-  }
-  _setWidth = (widthSize) => {
-    this.setState({width: widthSize});
-  }
-  _setHeight = (heightSize) => {
-    this.setState({height: heightSize});
-  }
-   render() {
+  render() {
     return (
       <div style={{paddingTop: '50px'}}>
         <table style={{margin: 'auto'}}>
@@ -25,10 +11,10 @@ class Main extends Component {
             <tr>
               <td>
                 <Map 
-                row={this.state.row} 
-                col={this.state.col} 
-                width={this.state.width}
-                height={this.state.height}
+                row={this.props.row} 
+                col={this.props.col} 
+                width={this.props.width}
+                height={this.props.height}
                 setBlockType={this.props.setBlockType}
                 getBlockType={this.props.getBlockType}
                 mapSet={this.props.mapSet}
@@ -36,15 +22,14 @@ class Main extends Component {
               </td>
               <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
               <td><Menu
-                row={this.state.row} 
-                col={this.state.col} 
-                width={this.state.width}
-                height={this.state.height}
-                border={this.state.border}
-                setRow={this._setRow}
-                setCol={this._setCol}
-                setWidth={this._setWidth}
-                setHeight={this._setHeight}
+                row={this.props.row} 
+                col={this.props.col} 
+                width={this.props.width}
+                height={this.props.height}
+                setRow={this.props.setRow}
+                setCol={this.props.setCol}
+                setWidth={this.props.setWidth}
+                setHeight={this.props.setHeight}
                 mapList={this.props.mapList}
                 updateMapSet={this.props.updateMapSet}
                 /></td>
