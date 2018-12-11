627648b625484d941e15e3b36606020126f1597a
Ian J.Um <31336310+code0xff@users.noreply.github.com>
2018-12-11 13:35:22 +0900

    Update AssetList.js

--- src/AssetList.js
+++ src/AssetList.js
@@ -2,12 +2,15 @@ import React, {Component} from 'react';
 import './AssetList.css';
 
 class AssetList extends Component {
+  _selectType = (e) => {
+    this.props.setImageSource('/images/' + this.props.mapSetList[e.target.id] + '.jpg');
+  }
+  
   _showMapList = () => {
     let mapList = this.props.mapList;
-
     return (mapList.map((type) => {
-      return <div key={type}>
-      <img alt={type} className='mapListImage' src={'/images/' + this.props.mapSetList[type] + '.jpg'}></img>&nbsp;
+      return <div className='AssetListContent' id={type} key={type} onClick={this._selectType}>
+      <img alt={type} className='MapListImage' src={'/images/' + this.props.mapSetList[type] + '.jpg'}></img>&nbsp;
       {type}</div>;
     }));
   }
