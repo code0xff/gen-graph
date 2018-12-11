4e5a5a2884dd8280d31833c069a0409dfa2a358a
Ian J.Um <31336310+code0xff@users.noreply.github.com>
2018-12-11 13:35:51 +0900

    Update AddAsset.js

--- src/AddAsset.js
+++ src/AddAsset.js
@@ -3,17 +3,15 @@ import './AddAsset.css';
 import {post} from 'axios';
 
 class AddAsset extends Component {
-  state = {assetName: '', assetFile: null, imageSource: ''}
-
   _onFormSubmit = (e) => {
     e.preventDefault();
 
-    if (this.state.assetName === '' || this.state.assetName === null) {
+    if (this.props.assetName === '' || this.props.assetName === null) {
       alert('please enter asset name.');
       return;
     }
 
-    if (this.state.assetFile === null) {
+    if (this.props.assetFile === null) {
       alert('please register file.');
       return;
     }
@@ -32,8 +30,8 @@ class AddAsset extends Component {
   _fileUpload = () => {
     const url = '/assets';
     const formData = new FormData();
-    formData.append('assetFile', this.state.assetFile);
-    formData.append('assetName', this.state.assetName);
+    formData.append('assetFile', this.props.assetFile);
+    formData.append('assetName', this.props.assetName);
     const config = {
       headers: {
         'content-type': 'multipart/form-data'
@@ -42,22 +40,8 @@ class AddAsset extends Component {
     return post(url, formData, config);
   }
 
-  _setAssetName = (e) => {
-    this.setState({assetName: e.target.value});
-  }
-
-  _setAssetFile = (e) => {
-    this.setState({assetFile: e.target.files[0]});
-
-    let fileReader = new FileReader();
-    fileReader.readAsDataURL(e.target.files[0]);
-    fileReader.onload = (e) => { 
-      this.setState({imageSource: e.target.result});
-    }
-  }
-
   _setAssetImage = () => {
-    return <img alt='empty' className='Preview' src={this.state.imageSource === '' ? '/images/empty.jpg' : this.state.imageSource}></img>
+    return <img alt='empty' className='Preview' src={this.props.imageSource === '' ? '/images/empty.jpg' : this.props.imageSource}></img>
   }
 
   render() {
@@ -70,11 +54,11 @@ class AddAsset extends Component {
         <div >asset name</div>
         <form onSubmit={this._onFormSubmit}>
           <div>
-            <input id='assetName' onChange={this._setAssetName} type='text'></input>
+            <input id='assetName' onChange={this.props.setAssetName} type='text'></input>
           </div>
           <br/>
           <div>
-            <input type='file' onChange={this._setAssetFile} /><button type='submit'>add </button>
+            <input type='file' onChange={this.props.setAssetFile} /><button type='submit'>add </button>
           </div>
         </form>
       </div>
