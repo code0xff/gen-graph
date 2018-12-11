c0d6700e1f9ceb68d0568ed836e39575a5e65b23
Ian J.Um <31336310+code0xff@users.noreply.github.com>
2018-12-11 15:23:45 +0900

    Update AssetList.js

--- src/AssetList.js
+++ src/AssetList.js
@@ -1,15 +1,38 @@
 import React, {Component} from 'react';
 import './AssetList.css';
+import axios from 'axios';
 
 class AssetList extends Component {
   _selectType = (e) => {
     this.props.setImageSource('/images/' + this.props.mapSetList[e.target.id] + '.jpg');
+  }
+
+  deleteList = [];
+  _checkDeleteType = (e) => {
+    if (e.target.checked) {
+      this.deleteList.push(e.target.id);
+    } else {
+      let index = this.deleteList.indexOf(e.target.id);
+      this.deleteList.splice(index, 1);
+    }
+  }
+
+  _deleteMapChip = () => {
+    axios.delete('/assets', {data: {deleteList: this.deleteList}})
+    .then(res => {
+      if (res.data.result === 'success') {
+        alert('success');
+        this.props.reloadMapList();
+      }
+    })
+    .catch(err => {console.log(err)});
   }
   
   _showMapList = () => {
     let mapList = this.props.mapList;
     return (mapList.map((type) => {
       return <div className='AssetListContent' id={type} key={type} onClick={this._selectType}>
+      <input id={type} type='checkbox' onClick={this._checkDeleteType}></input>
       <img alt={type} className='MapListImage' src={'/images/' + this.props.mapSetList[type] + '.jpg'}></img>&nbsp;
       {type}</div>;
     }));
@@ -21,6 +44,8 @@ class AssetList extends Component {
         <div className='AssetList'>
           {this._showMapList()}
         </div>
+        <br/>
+        <button style={{width: '200px'}} onClick={this._deleteMapChip}>delete</button>
       </div>
     )
   }
