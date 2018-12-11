a667faa14430969f17c47f3502274893485a3642
Ian J.Um <31336310+code0xff@users.noreply.github.com>
2018-12-11 13:36:14 +0900

    Update Asset.js

--- src/Asset.js
+++ src/Asset.js
@@ -3,6 +3,25 @@ import AddAsset from './AddAsset';
 import AssetList from './AssetList';
 
 class Asset extends Component {
+  state = {assetName: '', assetFile: null, imageSource: ''}
+
+  _setAssetName = (e) => {
+    this.setState({assetName: e.target.value});
+  }
+
+  _setAssetFile = (e) => {
+    this.setState({assetFile: e.target.files[0]});
+
+    let fileReader = new FileReader();
+    fileReader.readAsDataURL(e.target.files[0]);
+    fileReader.onload = (e) => { 
+      this._setImageSource(e.target.result);
+    }
+  }
+  _setImageSource = (source) => {
+    this.setState({imageSource: source});
+  }
+
   render() {
     return (
       <div style={{paddingTop: '50px'}}>
@@ -12,6 +31,11 @@ class Asset extends Component {
               <td>
                 <AddAsset 
                 reloadMapList={this.props.reloadMapList}
+                assetName={this.state.assetName}
+                assetFile={this.state.assetFile}
+                imageSource={this.state.imageSource}
+                setAssetName={this._setAssetName}
+                setAssetFile={this._setAssetFile}
                 />
               </td>
               <td>
@@ -21,6 +45,7 @@ class Asset extends Component {
                 <AssetList 
                 mapList={this.props.mapList}
                 mapSetList={this.props.mapSetList}
+                setImageSource={this._setImageSource}
                 />
               </td>
             </tr>
